11512c94550dd7017e1223769b35f6aad33d1412
Ahmed Nawaz Gondal <nawaz07210721@gmail.com>
2024-05-05 12:28:14 +0500

    clock done

---
+++ app.js
@@ -0,0 +1,56 @@
+function clock() {
+    var secDots = document.getElementById('secDots');
+    var minDots = document.getElementById('minDots');
+    var hrDots = document.getElementById('hrDots');
+    
+    var date = new Date();
+    var hours = date.getHours() % 12; // convert to 12-hour format
+    var amPm = date.getHours() >= 12 ? 'PM' : 'AM';
+    hours = hours === 0 ? 12 : hours; // handle midnight (0 hours)
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+
+    var secondsDots = '';
+    for (var i = 1; i < 61; i++) {
+        var rotation = i * 6; // rotate each line by 6 degrees
+        if (i === seconds) {
+            secondsDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"></div>';
+        } else {
+            secondsDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"></div>';
+        }
+    }
+
+    var minutesDots = '';
+    for (var i = 1; i < 61; i++) {
+        var rotation = i * 6; // rotate each line by 6 degrees
+        if (i === minutes) {
+            minutesDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"></div>';
+        } else {
+            minutesDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"></div>';
+        }
+    }
+
+    var hoursDots = '';
+    for (var i = 1; i < 13; i++) {
+        var rotation = i * 30; // rotate each line by 30 degrees
+        if (i === hours) {
+            hoursDots += '<div class="dot active" style="transform: rotate(' + rotation + 'deg)"></div>';
+        } else {
+            hoursDots += '<div class="dot" style="transform: rotate(' + rotation + 'deg)"></div>';
+        }
+    }
+
+    secDots.innerHTML = secondsDots + '<b>' + amPm + '</b>' + '<h2>' + zero(seconds) + '<br><span> Seconds </span></h2>';
+    minDots.innerHTML = minutesDots + '<h2>' + zero(minutes) + '<br><span> Minutes </span></h2>';
+    hrDots.innerHTML = hoursDots + '<h2>' + zero(hours) + '<br><span> Hours </span></h2>';
+}
+
+// add zero in single digital number
+function zero(number) {
+    if (number < 10) {
+        return '0' + number;
+    }
+    return number;
+}
+
+setInterval(clock, 1000);
